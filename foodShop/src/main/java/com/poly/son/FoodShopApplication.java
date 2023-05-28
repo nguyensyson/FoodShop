195bdd79f515096c11f97002777fa4357960f626
nguyensyson <ssnguyenson5671@gmail.com>
2023-05-28 23:00:58 +0700

    food shop spring boot

---
+++ foodShop/src/main/java/com/poly/son/FoodShopApplication.java
@@ -0,0 +1,13 @@
+package com.poly.son;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodShopApplication.class, args);
+	}
+
+}
---
+++ foodShop/src/test/java/com/poly/son/FoodShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.son;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
